82fa186caf8d7b44307f8bad6963bda938e6e958
MauroRaya <maurorayabr@gmail.com>
2025-09-17 09:47:53 -0300

    feat: initial commit

---
+++ src/test/java/br/unisanta/connexa/ConnexaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unisanta.connexa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnexaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
